a515812cb0430210292af6d3d86f098b82746f1a
Yui Tatsumi <yui.t@fuji.waseda.jp>
2023-09-25 01:49:53 +0900

    done creating the base part

--- splitApp.js
+++ splitApp.js
@@ -1,10 +1,7 @@
 $("#split").click(function () {  //クリックされたときに実行することを定義
-    const bill = parseInt($("inputBill").val());  //テキストボックスからテキストを取得し変数に格納
-    const people = parseInt($("inputPeople").val());
-    if (!isNaN(bill) && !isNaN(people)) {
-        const splitedBill = bill / people; // 請求額を人数で均等に割る
-        $("#splitedBill").text(splitedBill); // 結果を表示（小数点以下2桁まで表示）
-    } else {
-        $("#splitedBill").text("入力が無効です"); // 数値に変換できなかった場合のエラーメッセージ
-    }
+    const bill = $("#inputBill").val(); // テキストボックスのvalue値を取得
+    const people = $("#inputPeople").val();
+    const splitedBill = bill / people;
+    $("#splitedBill").text(splitedBill); // spanタグに値を設定
+
 });
